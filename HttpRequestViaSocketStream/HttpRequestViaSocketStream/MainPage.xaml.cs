51847e730f4375810c47a7a619c0e5afec7969f5
Christoph Wille <christoph.wille@gmail.com>
2013-09-02 18:03:18 +0200

    SocketStream sample (really, just a sample, do not use in production) to perform a HTTP request

---
+++ HttpRequestViaSocketStream/HttpRequestViaSocketStream/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace HttpRequestViaSocketStream
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private async void DoTheDemo_Click(object sender, RoutedEventArgs e)
+        {
+            string response = await GetUrlViaSocketStream(new Uri("http://www.orf.at/"));
+            Output.Text = response;
+        }
+
+        private async Task<string> GetUrlViaSocketStream(Uri url)
+        {
+            try
+            {
+                using (var socket = new StreamSocket())
+                {
+                    var host = new HostName(url.Host);
+
+                    await socket.ConnectAsync(host, "80").AsTask().ConfigureAwait(false);
+
+                    const string request = "GET {0} HTTP/1.1\r\n" +
+                                           "Host: {1}\r\n" +
+                                           "\r\n";
+
+                    var outStream = socket.OutputStream.AsStreamForWrite();
+                    using (var sw = new StreamWriter(outStream))
+                    {
+                        var requestToSend = String.Format(request, url.AbsoluteUri, url.Host);
+                        await sw.WriteAsync(requestToSend).ConfigureAwait(false);
+                        await sw.FlushAsync().ConfigureAwait(false);
+                    }
+
+                    var inStream = socket.InputStream.AsStreamForRead();
+
+                    using (var reader = new StreamReader(inStream))
+                    {
+                        var response = await reader.ReadToEndAsync().ConfigureAwait(false);
+                        return response;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+
+            return null;
+        }
+    }
+}
